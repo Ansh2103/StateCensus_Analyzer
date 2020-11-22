b1ef0ecfe8774cd91f5ca27a7fc5c4ca66cfe776
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-22 18:58:23 +0530

    [SHUBHAM] Added : Functionality to test and validate Wron India satate census CSV file

--- test_CensusAnalyzer/test_census_analyzer.py
+++ test_CensusAnalyzer/test_census_analyzer.py
@@ -4,7 +4,6 @@ from Main_CensusAnalyzer.CensusAnalyser_Exception import WrongFilePathError, Wro
 
 INDIA_STATE_CENSUS_PATH = '/home/shubh/PycharmProjects/IndianStateCensusAnalyzer/Main_CensusAnalyzer/IndiaStateCensusData.csv'
 WRONG_CSV_FILE = '/home/shubh/PycharmProjects/test_UserRegistration/IndiaStateCensusData.csv'
-WRONG_FILE_TYPE = '/home/shubh/PycharmProjects/IndianStateCensusAnalyzer/Main_CensusAnalyzer/IndiaStateCensusData.json'
 
 def test_load_census_data(instance_of_main_class):
     count_of_entries = instance_of_main_class.load_census_data(INDIA_STATE_CENSUS_PATH)
@@ -13,7 +12,3 @@ def test_load_census_data(instance_of_main_class):
 def test_wrong_filepath(instance_of_main_class):
     with pytest.raises(WrongFilePathError):
         instance_of_main_class.load_census_data(WRONG_CSV_FILE)
-
-def test_file_type(instance_of_main_class):
-    with pytest.raises(WrongExtensionFile):
-        instance_of_main_class.load_census_data(WRONG_FILE_TYPE)
