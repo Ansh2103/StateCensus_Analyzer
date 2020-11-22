12f431bbf96ff6980e6c39a3e5d1c598e9d9525f
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-22 19:14:41 +0530

    [SHUBHAM] Added : Functionality to test and validate Incorrect Delimiter

--- test_CensusAnalyzer/test_census_analyzer.py
+++ test_CensusAnalyzer/test_census_analyzer.py
@@ -5,6 +5,7 @@ from Main_CensusAnalyzer.CensusAnalyser_Exception import WrongFilePathError, Wro
 INDIA_STATE_CENSUS_PATH = '/home/shubh/PycharmProjects/IndianStateCensusAnalyzer/Main_CensusAnalyzer/IndiaStateCensusData.csv'
 WRONG_CSV_FILE = '/home/shubh/PycharmProjects/test_UserRegistration/IndiaStateCensusData.csv'
 WRONG_FILE_TYPE = '/home/shubh/PycharmProjects/IndianStateCensusAnalyzer/Main_CensusAnalyzer/IndiaStateCensusData.json'
+WRONG_DELIMITER_PATH = '/home/shubh/PycharmProjects/test_UserRegistration/delimeter_census_data.csv'
 
 
 def test_load_census_data(instance_of_main_class):
@@ -18,3 +19,9 @@ def test_wrong_filepath(instance_of_main_class):
 def test_file_type(instance_of_main_class):
     with pytest.raises(WrongExtensionFile):
         instance_of_main_class.load_census_data(WRONG_FILE_TYPE)
+
+def test_wrong_delimiter_filepath(instance_of_main_class):
+    with pytest.raises(WrongFilePathError):
+        instance_of_main_class.load_census_data(WRONG_DELIMITER_PATH)
+
+
