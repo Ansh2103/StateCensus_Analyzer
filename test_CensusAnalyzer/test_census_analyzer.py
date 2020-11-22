be66f1b8b0467948479ff4a122d6aa6bde4e13f3
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-22 20:46:40 +0530

    [SHUBHAM] Added : Functionality to test and validate Wrong file path raises custom exception

--- test_CensusAnalyzer/test_census_analyzer.py
+++ test_CensusAnalyzer/test_census_analyzer.py
@@ -8,7 +8,7 @@ WRONG_FILE_TYPE = '/home/shubh/PycharmProjects/IndianStateCensusAnalyzer/Main_Ce
 WRONG_DELIMITER_PATH = '/home/shubh/PycharmProjects/test_UserRegistration/delimeter_census_data.csv'
 WRONG_HEADER_PATH = '/home/shubh/PycharmProjects/test_UserRegistration/MissingHeader_StateCensusData.csv'
 INDIA_STATE_CODE_PATH = '/home/shubh/PycharmProjects/IndianStateCensusAnalyzer/Main_CensusAnalyzer/IndiaStateCode.csv'
-
+WRONG__STATE_CODE_CSV_PATH = '/home/shubh/PycharmProjects/StateCensusAnalyzer/test_StateCensusAnalyzer/IndiaStateCode.csv'
 def test_load_census_data(instance_of_main_class):
     number_of_records = instance_of_main_class.load_census_data(INDIA_STATE_CENSUS_PATH)
     assert number_of_records == 28
@@ -32,3 +32,8 @@ def test_wrong_header_filepath(instance_of_main_class):
 def test_load_census_code_data(instance_of_main_class):
     number_of_records = instance_of_main_class.load_census_data(INDIA_STATE_CODE_PATH)
     assert number_of_records == 36
+
+def test_wrong_state_census_code_filepath(instance_of_main_class):
+    with pytest.raises(WrongFilePathError):
+        instance_of_main_class.load_census_data(WRONG__STATE_CODE_CSV_PATH)
+
