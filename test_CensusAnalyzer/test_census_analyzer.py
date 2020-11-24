aecb2d4496359449cc56cf5699327890d509b2cc
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-24 06:25:30 +0530

    [SHUBHAM] Refactor : Implementing DRY Principle by using Parameterized testing

--- test_CensusAnalyzer/test_census_analyzer.py
+++ test_CensusAnalyzer/test_census_analyzer.py
@@ -17,41 +17,29 @@ def test_load_census_data(instance_of_main_class):
     number_of_records = instance_of_main_class.load_census_data(INDIA_STATE_CENSUS_CSV_PATH)
     assert number_of_records == 28
 
+@pytest.mark.parametrize("file_path , expected" , [
+    (WRONG_INDIA_STATE_CSV_FILE , WrongFilePathError),
+    (WRONG_FILE_TYPE , WrongExtensionFile),
+    (WRONG__STATE_CODE_CSV_PATH , WrongFilePathError),
+    (WRONG_STATE_CODE_FILE_TYPE , WrongExtensionFile),
+    (WRONG_DELIMITER_PATH , WrongFilePathError),
+    (WRONG_HEADER_PATH , WrongFilePathError)
+])
+def test_wrong_file_path_must_raise_exception(instance_of_main_class ,file_path , expected ):
+    with pytest.raises(expected):
+        instance_of_main_class.load_census_data(file_path)
 
-def test_wrong_filepath(instance_of_main_class):
-    with pytest.raises(WrongFilePathError):
-        instance_of_main_class.load_census_data(WRONG_INDIA_STATE_CSV_FILE)
 
-
-def test_file_type(instance_of_main_class):
-    with pytest.raises(WrongExtensionFile):
-        instance_of_main_class.load_census_data(WRONG_FILE_TYPE)
 
 def test_load_census_delimiter_data(instance_of_main_class):
     number_of_records = instance_of_main_class.load_census_delimiter_data(DELIMITER_CENSUS_DATA)
     assert number_of_records == 28
 
 
-def test_wrong_delimiter_filepath(instance_of_main_class):
-    with pytest.raises(WrongFilePathError):
-        instance_of_main_class.load_census_data(WRONG_DELIMITER_PATH)
-
-
-def test_wrong_header_filepath(instance_of_main_class):
-    with pytest.raises(WrongFilePathError):
-        instance_of_main_class.load_census_data(WRONG_HEADER_PATH)
-
-
 def test_load_census_code_data(instance_of_main_class):
     number_of_records = instance_of_main_class.load_census_data(INDIA_STATE_CODE_PATH)
     assert number_of_records == 36
 
-
-def test_wrong_state_census_code_filepath(instance_of_main_class):
-    with pytest.raises(WrongFilePathError):
-        instance_of_main_class.load_census_data(WRONG__STATE_CODE_CSV_PATH)
+#
 
 
-def test_wrong_state_code_file_type(instance_of_main_class):
-    with pytest.raises(WrongExtensionFile):
-        instance_of_main_class.load_census_data(WRONG_STATE_CODE_FILE_TYPE)
